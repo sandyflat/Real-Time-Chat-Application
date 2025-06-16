5a2394a954869a560d1eefc76f05343842afd525
sandyflat <chapsandip17@gmail.com>
2025-06-16 17:56:31 +0545

    first commit

---
+++ src/main/java/com/sandyflat/ChatApplication/controller/ChatController.java
@@ -0,0 +1,22 @@
+package com.sandyflat.ChatApplication.controller;
+
+import com.sandyflat.ChatApplication.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage chatMessage){
+        return chatMessage;
+    }
+
+    @GetMapping("chat")
+    public String chat(){
+        return "chat";
+    }
+}
